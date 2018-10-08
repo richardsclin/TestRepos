15b38ce2b31feebdbaae17c442e0662bdf4baa71
shihching.lin <shihching.lin@fubon.com>
2018-10-08 16:03:59 +0800

    AAAAA

--- WebApplication3/Global.asax.cs
+++ WebApplication3/Global.asax.cs
@@ -16,7 +16,7 @@ namespace WebApplication3
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);   
             BundleConfig.RegisterBundles(BundleTable.Bundles);     
         }
     }
